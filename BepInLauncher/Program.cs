73e76d3db307a37b16c8875fff2e2f1eaac1b033
denikson <geoffrey.hoooooorse@gmail.com>
2018-05-31 15:49:38 +0300

    Initial commit

---
+++ BepInLauncher/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SimpleJSON;
+
+namespace BepInLauncher
+{
+    class Program
+    {
+        public static string GamePath { get; set; }
+
+        static void Main(string[] args)
+        {
+            GamePath = args[0];
+
+            Console.WriteLine("Creating file system tree");
+            CreateFileSystemTree();
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+            Console.WriteLine("Launching the game!");
+            LaunchGame();
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+
+        static void LaunchGame()
+        {
+            Process p = Process.Start(GamePath, "--doorstop-enable true --doorstop-target \"BepInEx\\bin\\BepInPreloader.dll\"");
+        }
+
+        static void CreateFileSystemTree()
+        {
+            JSONObject o = new JSONObject();
+
+            o["BepInEx"] = new JSONObject();
+
+            GenTree(o["BepInEx"].AsObject, "BepInEx");
+
+            foreach (string modDir in Directory.GetDirectories("mods"))
+                GenTree(o, modDir);
+
+            StringBuilder sb = new StringBuilder();
+
+            o.WriteToStringBuilder(sb, 4, 1, JSONTextMode.Compact);
+
+            File.WriteAllText("vfs.json", sb.ToString());
+        }
+
+        static void GenTree(JSONObject treeRoot, string path)
+        {
+            // TODO: Handle ambiguities
+
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                string dirName = Path.GetFileName(directory);
+
+                JSONObject subdir = treeRoot[dirName].AsObject;
+                GenTree(subdir, directory);
+            }
+
+            foreach (string file in Directory.GetFiles(path))
+            {
+                string fileName = Path.GetFileName(file);
+
+                treeRoot[fileName] = file;
+            }
+        }
+    }
+}
---
+++ VirtualFS/VirtualFS.h
@@ -0,0 +1,78 @@
+#pragma once
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
+#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#include <windows.h>
+#include "json.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/minhook/include/MinHook.h"
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <regex>
+#include <stack>
+#include <algorithm>
+#include <direct.h>
+#include <experimental/filesystem>
+#include <list>
+
+#define DllExport __declspec( dllexport )
+
+#define BUFSIZE 4096
+#define DEBUG false
+
+#if DEBUG
+#define print(x, y) _cwprintf_s(x, y)
+#else
+#define print(x, y)
+#endif
+
+
+//Represents a "result" of a filesystem query (FindFirstFile/FindNextFile).
+typedef struct FileInfo
+{
+	//WIN32_FIND_DATAW provides more information than name/attributes, but this seems sufficient.
+	std::wstring cFileName = L"";
+	DWORD dwFileAttributes = 0;
+
+	//Operators used for sorting.
+	bool operator==(const FileInfo& f) const { return cFileName == f.cFileName; }
+	bool operator<(const FileInfo& f) const { return cFileName < f.cFileName; }
+
+	FileInfo()
+	{
+	}
+} FileInfo;
+
+//We don't have any way to determine the original parameters when hooking FindNextFile.
+//By establishing a HANDLE -> Metadata mapping, we can better handle the hook.
+//No longer needs to be a struct after being slimmed down.
+typedef struct HandleMap
+{
+	std::list<FileInfo> Results;
+
+	HandleMap()
+	{
+	}
+} HandleMap;
+
+
+// Definitions for the original functions
+
+BOOL (WINAPI* TrueFindClose)(HANDLE hFindFile);
+
+HANDLE (WINAPI* TrueFindFirstFileW)(LPCWSTR lpFileName, WIN32_FIND_DATAW* lpFindFileData);
+
+BOOL (WINAPI* TrueFindNextFileW)(HANDLE hFindFile, WIN32_FIND_DATAW* lpFindFileData);
+
+BOOL (WINAPI* TrueGetFileAttributesExW)(LPCWSTR lpFileName, GET_FILEEX_INFO_LEVELS fInfoLevelId,
+                                       LPVOID lpFileInformation);
+
+DWORD (WINAPI* TrueGetFileAttributesW)(LPCWSTR lpFileName);
+
+HANDLE (WINAPI* TrueCreateFileW)(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode,
+                                 LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition,
+                                 DWORD dwFlagsAndAttributes, HANDLE hTemplateFile);
